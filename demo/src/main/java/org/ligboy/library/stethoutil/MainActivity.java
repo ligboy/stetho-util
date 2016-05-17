24e6f41b0bd7309c9f6de0899e231e5ebfbe411a
ligboy <ligboy.liu@heysroad.com>
2016-05-17 12:49:20 +0800

    ignore invalid package

--- demo/src/main/java/org/ligboy/library/stethoutil/MainActivity.java
+++ demo/src/main/java/org/ligboy/library/stethoutil/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import timber.log.Timber;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,10 +22,24 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Timber.tag("MainActivity").i("Replace with your own action");
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+        Timber.tag("MainActivity").i("onCreate");
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Timber.tag("MainActivity").i("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Timber.tag("MainActivity").i("onPause");
+    }
 }
